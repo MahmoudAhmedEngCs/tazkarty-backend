f216bf8a4083bae31bfc556188467029c39931d2
MahmoudAhmedEngCs <mahmoud.ahmed.cs.eng@gmail.com>
2026-09-10 18:14:34 +0300

    Prepare API for deployment

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import app from "./app";
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
